713ea4cc6ae61ee615ca895accac0d5256a8370a
MalikZone <ridwanmalik2010@gmail.com>
2019-12-10 16:36:04 +0700

    mau nambahin juery tapi gak jadi

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('#img').tooltip('focus');
+$('#img2').tooltip('focus');
+
+
+
+// $(document).ready(function() {
+//   $("#a").click(function() {
+//     $("#img").fadeIn("slow", 0.4);
+//   });
+// });
